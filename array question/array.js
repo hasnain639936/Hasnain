33f744c465735fbb4b165db2338ae19255cae7ae
hasnain639936 <hasnain639936@gmail.com>
2024-05-14 23:18:13 -0700

    array  question

---
+++ array question/array.js
@@ -0,0 +1,6 @@
+let arr=["bloomberg","microsoft","Uber","google","IBM","NETFLIX"];
+console.log(arr);
+arr.shift();
+arr.push("Amazon");
+arr.splice(2,1,"Ola");
+console.log(arr);
--- push.js
+++
@@ -1,4 +0,0 @@
-let marvel=["thore","ironman","captan","Dr.strang","superman"];
-console.log(marvel);
-marvel.splice(2,1);
-console.log(marvel);
